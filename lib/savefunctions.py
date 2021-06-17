7a033793c01b8a955552a5cd7c974b6588ae7494
collbrown <collin-brown@uiowa.edu>
2021-06-17 11:17:13 -0500

    added build params

--- lib/savefunctions.py
+++ lib/savefunctions.py
@@ -165,6 +165,13 @@ def parse_input_file(path):
     Warning: this function is ad hoc and doesn't work for all input parameters,
     but it works for the ones we need it to
 
+    Warning: this does not properly label injectors if there are multiple
+    injectors for multiple species
+
+    Warning: this function does not handle commenting out variable assignments well
+    (i.e. avoiding leaving '!var=val0,val1,...' or '!C!var=val0,val1,...' in input
+    file)
+
     Parameters
     ----------
     path : string
@@ -183,9 +190,9 @@ def parse_input_file(path):
             #get name of each block of input parameters
             contents = line.split(' ')
             speciescounter = 1 #used to handle having multiple species
+            injectorcounter = 1 #used to handle having multiple injectors
             if(len(contents) == 1 and contents[0] != '\n' and contents[0] != '\t\n' and not('{' in contents[0]) and not('}' in contents[0]) and not('!' in contents[0])):
                 blockname = contents[0].split('\n')[0]+'_'
-
 
             #check if line contains equal sign
             if '=' in line:
@@ -197,7 +204,7 @@ def parse_input_file(path):
                             vals_out = []
                             for k in range(0,len(vals)):
                                 vals[k] = vals[k].replace('"','')
-                                if(vals[k].isnumeric()):
+                                if(vals[k].isnumeric() or _isfloat(vals[k])):
                                     vals_out.append(float(vals[k]))
                                 elif(not(vals[k]=='' or '\n' in vals[k] or '\t' in vals[k])):
                                     vals_out.append(vals[k])
@@ -206,7 +213,10 @@ def parse_input_file(path):
                                 if(blockname+str(speciescounter)+'_'+varvalpair[0] in d):
                                     speciescounter += 1
                                 d[blockname+str(speciescounter)+'_'+varvalpair[0]] = vals_out
-
+                            elif(blockname == 'plasma_injector_'):
+                                if(blockname+str(injectorcounter)+'_'+varvalpair[0] in d):
+                                    injectorcounter += 1
+                                d[blockname+str(injectorcounter)+'_'+varvalpair[0]] = vals_out
                             else:
                                 d[blockname+varvalpair[0]] = vals_out
                         else:
@@ -216,3 +226,179 @@ def parse_input_file(path):
                             d[blockname+varvalpair[0]] = [val]
 
     return d
+
+def build_params(inputdict, numframe):
+    """
+    Computes relevant inputs parameters from simulation
+
+
+    Parameters
+    ----------
+    inputdict : dict
+        dictionary containing input parameters from parse_input_file
+
+    numframe : int
+        frame number (i.e. what time slice) with are analyzing
+
+
+    Returns
+    -------
+    params : dict
+        dictionary containing global attributes relating to data.
+        contains mostly physical input parameters from original simulation
+    """
+
+    #relevant input data
+    dt = float(inputdict['time_dt'][0]) #inverse Omega_ci0
+    rqm = inputdict['species_1_rqm'][0] #charge to mass ratio (inverse)
+    qi = 1./rqm
+
+    #TODO check that species vth matches injector vth
+    #assumes species 1 is ions. Not sure how to check this
+    if(inputdict['species_1_vth'][0] == inputdict['plasma_injector_1_vth'][0]):
+        vti = inputdict['species_1_vth'][0]
+    else:
+        print("Warning: injector vth does not match species vti")
+
+    betaion = vti**2. #vti is normalized to v_alfven
+    betaelec = 1. #TODO: figure out this
+
+    Bx = inputdict['ext_emf_Bx'][0]
+    By = inputdict['ext_emf_By'][0]
+    Bz = inputdict['ext_emf_Bz'][0]
+    if(Bx != 0.):
+        shocknormalangle = abs(math.atan((By**2.+Bz**2.)**0.5/Bx))*360./(2.0*math.pi)
+    else:
+        shocknormalangle = 0.
+
+    #define attributes/ simulation parameters
+    params = {}
+    params["MachAlfven"] = float('nan')
+    params["MachAlfvenNote"] = 'TODO: compute mach alfven for this run'
+    params["ShockNormalAngle"] = shocknormalangle
+    params["ShockNormalAngledesc"] = 'units of degrees'
+    params["betaelec"] = betaelec
+    params["betaion"] = betaion
+    params["simtime"] = numframe*dt
+    params["simtimedesc"] = 'units of inverse Omega_{c,i,0}'
+    params["qi"] = qi
+    params["qidesc"] = 'charge to mass ratio'
+    params["di"] = 0.0
+    params["didesc"] = 'TODO: compute ion inertial length'
+    params["vti"] = vti
+
+    return params
+
+def read_input(path='./'):
+    """
+    Alternative parse dHybrid input file for simulation information (by Dr. Haggerty)
+
+    Parameters
+    ----------
+    path : str
+        path of input file
+
+    Returns
+    -------
+    dict : dict
+        dictionary containing input parameters
+    """
+    import os
+    path = os.path.join(path, "input/input")
+    inputs = {}
+    repeated_sections = {}
+    # Load in all of the input stuff
+    with open(path) as f:
+        in_bracs = False
+        for line in f:
+            # Clean up string
+            line = line.strip()
+            # Remove comment '!'
+            trim_bang = line.find('!')
+            if trim_bang > -1:
+                line = line[:trim_bang].strip()
+            # Is the line not empty?
+            if line:
+                if not in_bracs:
+                    in_bracs = True
+                    current_key = line
+                    # The input has repeated section and keys for differnt species
+                    # This section tries to deal with that
+                    sp_counter = 1
+                    while current_key in inputs:
+                        inputs[current_key+"_01"] = inputs[current_key]
+                        sp_counter += 1
+                        current_key = "{}_{:02d}".format(line, sp_counter)
+                        repeated_sections[current_key] = sp_counter
+                    inputs[current_key] = []
+                else:
+                    if line == '{':
+                        continue
+                    elif line == '}':
+                        in_bracs = False
+                    else:
+                        inputs[current_key].append(line)
+    # Parse the input and cast it into usefull types
+    param = {}
+    repeated_keys = {}
+    for key,inp in inputs.items():
+        for sp in inp:
+            k = sp.split('=')
+            k,v = [v.strip(' , ') for v in k]
+            _fk = k.find('(')
+            if _fk > 0:
+                k = k[:_fk]
+            if k in param:
+                param["{}_{}".format(k, key)] = param[k]
+                k = "{}_{}".format(k, key)
+            param[k] = [_auto_cast(c.strip()) for c in v.split(',')]
+            if len(param[k]) == 1:
+                param[k] = param[k][0]
+    return param
+
+def _auto_cast(k):
+    """
+    Takes an input string and tries to cast it to a real type (by Dr. Haggerty)
+
+    Helper function for read_input
+
+    Parameters
+    ----------
+    k : str
+        A string that might be a int, float or bool
+
+    Returns
+    -------
+
+    """
+    k = k.replace('"','').replace("'",'')
+    for try_type in [int, float]:
+        try:
+            return try_type(k)
+        except:
+            continue
+    if k == '.true.':
+        return True
+    if k == '.false.':
+        return False
+    return str(k)
+
+def _isfloat(value):
+    """
+    Checks if string is float
+
+    Parameters
+    ----------
+    value : str
+        string you want to check if its a float
+
+    Returns
+    -------
+    bool
+        true if float
+    """
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
