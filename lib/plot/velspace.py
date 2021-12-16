5116194eef43f81e293f6e30f65cf506cce34d26
Collin Brown <collin.crbrown@gmail.com>
2021-12-16 13:47:02 -0600

    work on wavemodes and sweep plot

--- lib/plot/velspace.py
+++ lib/plot/velspace.py
@@ -634,8 +634,8 @@ def plot_cor_and_dist_supergrid(vx, vy, vz, vmax,
     #set ticks
     intvl = 10.
     tcks = np.arange(0,vmax+intvl,intvl)
-    if(tcks[:-1] > vmax):
-        del tcks[:-1]
+    if(tcks[-1] > vmax):
+        del tcks[-1]
     tcks = np.concatenate((-1*np.flip(tcks),tcks))
     for _i in range(0,4):
         for _j in range(0,3):
