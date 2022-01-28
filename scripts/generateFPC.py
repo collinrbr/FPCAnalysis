17d8a20473141fd53120447cff0eeeac4d9e3346
Collin Brown <collin.crbrown@gmail.com>
2022-01-28 16:44:40 +0000

    work on presliced multi processing

--- scripts/generateFPC.py
+++ scripts/generateFPC.py
@@ -138,7 +138,8 @@ if(vmax == None):
 #-------------------------------------------------------------------------------
 # Check if data is 2D. Pad to make pseudo 3d if data is 2d
 #-------------------------------------------------------------------------------
-if(is_2D3V):
+#print(is_2D3V)
+if(is_2D3V == 'T'): #TODO: fix inconsistency (is_2D3V is either 'T'/'F' while is2D3V is either True or False) have one var and use only boolean
     if('x3' not in dparticles.keys()): #all simulations that are '2d' should be 2d 3v with coordinates (xx,yy;vx,vy,vz)
         dparticles = dh5.par_2d_to_3d(dparticles)
         dfields = dh5.dict_2d_to_3d(dfields,0)
