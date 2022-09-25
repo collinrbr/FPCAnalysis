8d603126701c21851e2c095de5a100ec44e3c1fd
collinrbr <36084936+collinrbr@users.noreply.github.com>
2022-09-25 16:59:16 -0500

    Update fpc.py to use shock frame for FPC analysis

--- lib/fpc.py
+++ lib/fpc.py
@@ -148,7 +148,7 @@ def compute_hist_and_cor(vmax, dv, x1, x2, y1, y2, z1, z2,
         gptsparticle = (x1 <= dpar['x1']) & (dpar['x1'] <= x2) & (y1 <= dpar['x2']) & (dpar['x2'] <= y2) & (z1 <= dpar['x3']) & (dpar['x3'] <= z2)
         try: #This is hacky TODO: clean this up by simply returning hist and cor arrays full of zeros
             dparsubset = {
-              'p1': dpar['p1'][gptsparticle][:],
+              'p1': dpar['p1'][gptsparticle][:]-vshock,
               'p2': dpar['p2'][gptsparticle][:],
               'p3': dpar['p3'][gptsparticle][:],
               'x1': dpar['x1'][gptsparticle][:],
