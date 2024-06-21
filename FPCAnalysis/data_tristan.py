fixed conversion between vthi and va error and factor of sqrt2 in tristan analysis particle frame transform functions. Tested by boosting fields/particles and comparing to e cross b drift

FPCAnalysis/data_tristan.py
@@ -222,8 +222,8 @@ def load_particles(path, num, normalizeVelocity=False,loaddebugsubset=False):
     if(normalizeVelocity):
 
         params = load_params(path,num)
-        massratio = load_params(path,num)['mi']/load_params(path,num)['me']
-        vti0 = np.sqrt(params['delgam'])#Note: velocity is in units γV_i/c so we do not include '*params['c']'
+        massratio = params['mi']/params['me']
+        vti0 = np.sqrt(params['delgam'])#Note: velocity is in units v_s/c
         vte0 = np.sqrt(params['mi']/params['me'])*vti0 #WARNING: THIS ASSUME Ti/Te = 1, TODO: don't assume Ti/Te = 1
         comp = params['comp']
 

FPCAnalysis/frametransform.py
@@ -97,7 +97,7 @@ def lorentz_transform_v(dfields, vx, vy, vz, c):
 
     #!!!!!Pressed for time and the magnetic fields aren't used whenever this function is used, so I will implement it later TODO: implement
     dfieldslor['bx'] = dfields['bx']
-    dfieldslor['by'] = gamma*(dfields['by']+vx/c**2*dfields['ez']) #TODO: check these units (i.e. should there be an extra 1/c factor?)
+    dfieldslor['by'] = gamma*(dfields['by']+vx/c**2*dfields['ez'])
     dfieldslor['bz'] = gamma*(dfields['bz']-vx/c**2*dfields['ey'])
 
     return dfieldslor
@@ -149,7 +149,7 @@ def shift_particles(dparticles, vx, beta_s):
 
     return dparticlestransform
 
-def shift_particles_tristan(dparticles, vx, beta, mi_me, isIon, Ti_Te = 1., galileanboost = True, c = None):
+def shift_particles_tristan(dparticles, vx, betai, betae, mi_me, isIon, Ti_Te = 1., galileanboost = True, c = None):
     """
     Transforms velocity frame of particles
 
@@ -160,6 +160,8 @@ def shift_particles_tristan(dparticles, vx, beta, mi_me, isIon, Ti_Te = 1., gali
     TODO: rewrite parameter inputs!
 
     c is in units of va (c_input = c/va (val of 3 means c is 3 times va)
+
+    Assumes mi >> me
 
     Parameters
     ----------
@@ -176,12 +178,13 @@ def shift_particles_tristan(dparticles, vx, beta, mi_me, isIon, Ti_Te = 1., gali
         for key in vxaliases:
             if(key in dparticles.keys()):
                 if(isIon):
-                    dparticlestransform[key] = dparticles[key] - vx / np.sqrt(beta)
+                    dparticlestransform[key] = dparticles[key] - vx np.sqrt(2) / np.sqrt(betai)
                 else:
-                    dparticlestransform[key] = dparticles[key] - vx * (1./(np.sqrt(beta)*(mi_me**0.5)))*(Ti_Te)
+                    dparticlestransform[key] = dparticles[key] - vx * (np.sqrt(2)/(np.sqrt(betae)*(mi_me**0.5)))*(Ti_Te)
         dparticlestransform['Vframe_relative_to_sim'] = dparticles['Vframe_relative_to_sim'] + vx
 
     else:
+        gamma = 1./np.sqrt(1.-(vx/c)**2)
         pass
 
     return dparticlestransform

scripts/generateFPCfromTristan.py
@@ -79,6 +79,7 @@ if __name__ == '__main__':
     dt,c = anl.norm_constants_tristanmp1(params,dt,inputs) #in units of wci (\omega_ci) and va respectively- as required by the rest of the scripts
 
     beta0 = anl.compute_beta0_tristanmp1(params,inputs)
+    betai,betae= anl.get_betai_betae_from_tot_and_ratio(beta0,params['temperature_ratio'])
 
     dfields = dtr.load_fields(path,num,normalizeFields=True)
 
@@ -92,7 +93,7 @@ if __name__ == '__main__':
 
     uinj_voverc = inputs['gamma0']
     uinj_vth = uinj_voverc/np.sqrt(params['delgam'])
-    params['MachAlfven'] =  uinj_vth*np.sqrt(beta0)*params['c'] #assumes me << mi
+    params['MachAlfven'] =  uinj_vth*np.sqrt(betai)*params['c'] #assumes me << mi
 
     #-------------------------------------------------------------------------------
     # estimate shock vel and lorentz transform
@@ -113,8 +114,8 @@ if __name__ == '__main__':
     dfields = ft.lorentz_transform_vx_c(dfields,vshock,c) #note: we only boost one frame
 
     if(num_threads == 1):
-        dpar_ion = ft.shift_particles_tristan(dpar_ion, vshock, beta0, params['mi']/params['me'], isIon=True)
-        dpar_elec = ft.shift_particles_tristan(dpar_elec, vshock, beta0, params['mi']/params['me'], isIon=False)
+        dpar_ion = ft.shift_particles_tristan(dpar_ion, vshock, betai, betae, params['mi']/params['me'], isIon=True)
+        dpar_elec = ft.shift_particles_tristan(dpar_elec, vshock, betai, betae, params['mi']/params['me'], isIon=False)
         dpar_ion = dtr.format_par_like_dHybridR(dpar_ion) #For now, we rename the particle data keys too look like the keys we used when processing dHybridR data so this data is compatible with our old routines
         dpar_elec = dtr.format_par_like_dHybridR(dpar_elec)
 
@@ -130,7 +131,7 @@ if __name__ == '__main__':
         CEx, CEy, CEz, x, Hist, vx, vy, vz, num_par = fpc.compute_correlation_over_x(dfields, dpar_ion, vmax, dv, dx, vshock, xlim, ylim, zlim)
         Histxy,Histxz,Histyz,CExxy,CExxz,CExyz,CEyxy,CEyxz,CEyyz,CEzxy,CEzxz,CEzyz = fpc.project_CEi_hist(Hist, CEx, CEy, CEz)
     else:
-        CExxy,CExxz,CExyz,CEyxy,CEyxz,CEyyz,CEzxy,CEzxz,CEzyz,x,Histxy,Histxz,Histyz, vx, vy, vz, num_par = fpc.comp_cor_over_x_multithread(dfields, dpar_folder_ion, vmax, dv, dx, vshock, xlim=xlim, ylim=ylim, zlim=zlim, max_workers=num_threads, beta0=beta0, mi_me=params['mi']/params['me'], isIon=True)
+        CExxy,CExxz,CExyz,CEyxy,CEyxz,CEyyz,CEzxy,CEzxz,CEzyz,x,Histxy,Histxz,Histyz, vx, vy, vz, num_par = fpc.comp_cor_over_x_multithread(dfields, dpar_folder_ion, vmax, dv, dx, vshock, xlim=xlim, ylim=ylim, zlim=zlim, max_workers=num_threads, betai=betai, betae=betae, mi_me=params['mi']/params['me'], isIon=True)
 
     #-------------------------------------------------------------------------------
     # compute energization
@@ -160,7 +161,7 @@ if __name__ == '__main__':
         CEx, CEy, CEz, x, Hist, vx, vy, vz, num_par = fpc.compute_correlation_over_x(dfields, dpar_elec, vmax, dv, dx, vshock, xlim, ylim, zlim)
         Histxy,Histxz,Histyz,CExxy,CExxz,CExyz,CEyxy,CEyxz,CEyyz,CEzxy,CEzxz,CEzyz = fpc.project_CEi_hist(Hist, CEx, CEy, CEz)
     else:
-        CExxy,CExxz,CExyz,CEyxy,CEyxz,CEyyz,CEzxy,CEzxz,CEzyz,x, Histxy,Histxz,Histyz, vx, vy, vz, num_par = fpc.comp_cor_over_x_multithread(dfields, dpar_folder_elec, vmax, dv, dx, vshock, xlim=xlim, ylim=ylim, zlim=zlim, max_workers=num_threads, beta0=beta0, mi_me=params['mi']/params['me'], isIon=True)
+        CExxy,CExxz,CExyz,CEyxy,CEyxz,CEyyz,CEzxy,CEzxz,CEzyz,x, Histxy,Histxz,Histyz, vx, vy, vz, num_par = fpc.comp_cor_over_x_multithread(dfields, dpar_folder_elec, vmax, dv, dx, vshock, xlim=xlim, ylim=ylim, zlim=zlim, max_workers=num_threads, betai=betai, betae=betae, mi_me=params['mi']/params['me'], isIon=True)
 
     #-------------------------------------------------------------------------------
     # compute energization